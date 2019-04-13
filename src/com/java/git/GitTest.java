efe77c5706f1b94164f8ad34abf0f4395121ccf8
Rajesh Pandey <rajeshkpy@gmail.com>
2019-04-13 15:35:38 +0530

    Added SOP to suggest creating a new Branch

--- src/com/java/git/GitTest.java
+++ src/com/java/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("this is git test project");
 		System.out.println("this is after making first commit");
+		System.out.println("This is after creating a branch");
 
 	}
 
