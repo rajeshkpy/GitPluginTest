3da1e22fb33e08b4f05ed0e325dd3ddafeedd53d
Rajesh Pandey <rajeshkpy@gmail.com>
2019-04-13 15:28:47 +0530

    Added another SOP statement

--- src/com/java/git/GitTest.java
+++ src/com/java/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("this is git test project");
+		System.out.println("this is after making first commit");
 
 	}
 
